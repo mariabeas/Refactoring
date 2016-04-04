82a1413e12373e72eff5d5ce26dc151b881ad913
mariabeas <mdm_beas@hotmail.com>
2016-04-04 18:10:30 +0200

    Refactoring

---
+++ src/org/pris2/refactoring/Customer.java
@@ -0,0 +1,16 @@
+package org.pris2.refactoring;
+
+class Customer {
+	private String _name;
+	private Vector _rentals = new Vector();
+
+	public Customer(String name) {
+		_name = name;
+	};
+
+	public void addRental(Rental arg) {
+		_rentals.addElement(arg);
+	}
+
+public String getName (){
+return _name; };
