85494a2874994d9f01ae0be82e184a47b09ef068
mariabeas <mdm_beas@hotmail.com>
2016-04-06 13:44:57 +0200

    Método getCharge pasandole los dias del alquiler de la clase Rental a la clase Movie Metodo getFrequentRenterPoints pasandole los dias del alquiler de la clase Rental a la clase movie

--- src/org/pris2/refactoring/Movie.java
+++ src/org/pris2/refactoring/Movie.java
@@ -2,28 +2,56 @@ package org.pris2.refactoring;
 
 public class Movie {
 
-public static final int CHILDRENS = 2;
-public static final int REGULAR = 0;
-public static final int NEW_RELEASE = 1;
+	public static final int CHILDRENS = 2;
+	public static final int REGULAR = 0;
+	public static final int NEW_RELEASE = 1;
 
-private String title;
-private int priceCode;
+	private String title;
+	private int priceCode;
 
-public Movie(String intitle, int inpriceCode) {
-	title = intitle;
-	priceCode = inpriceCode;
+	public Movie(String intitle, int inpriceCode) {
+		title = intitle;
+		priceCode = inpriceCode;
+	}
+
+	public int getPriceCode() {
+		return priceCode;
+	}
+
+	public void setPriceCode(int arg) {
+		priceCode = arg;
+	}
+
+	public String getTitle() {
+		return title;
+	};
+
+	double getCharge(int daysRented) {
+		double result = 0;
+		switch (getPriceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if (daysRented > 2)
+				result += (daysRented - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			result += daysRented * 3;
+			break;
+		case Movie.CHILDRENS:
+
+			result += 1.5;
+			if (daysRented > 3)
+				result += (daysRented - 3) * 1.5;
+			break;
+		}
+		return result;
+	}
+
+	int getFrequentRenterPoints(int daysRented) {
+		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+			return 2;
+		else
+			return 1;
+	}
+
 }
-
-public int getPriceCode() {
-	return priceCode;
-}
-
-public void setPriceCode(int arg) {
-	priceCode = arg;
-}
-
-public String getTitle (){
-	return title;
-};
-
-} 
